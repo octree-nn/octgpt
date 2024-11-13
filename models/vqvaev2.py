Fix kl in vae

models/vqvaev2.py
@@ -381,8 +381,6 @@ class DiagonalGaussian(object):
     if other is None:
       out = 0.5 * (torch.pow(self.mean, 2) + self.var - 1.0 - self.logvar)
     else:
-      out = 0.5 * torch.sum(
-          torch.pow(self.mean - other.mean, 2) / other.var +
-          self.var / other.var - 1.0 - self.logvar + other.logvar,
-          dim=1)
+      out = 0.5 * (torch.pow(self.mean - other.mean, 2) / other.var +
+                   self.var / other.var - 1.0 - self.logvar + other.logvar)
     return out