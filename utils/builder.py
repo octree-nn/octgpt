59f54521862c4642a3d345776aef246f492550a2
wang-ps <wangps@hotmail.com>
2024-11-25 14:46:49 +0800

    Add vqvae-huge

--- utils/builder.py
+++ utils/builder.py
@@ -36,12 +36,28 @@ def build_vae_model(flags):
       super().config_network()
       self.bottleneck = 1
 
+  class VQVAEh(VQVAE):
+    def config_network(self):
+      self.bottleneck = 1
+      self.mpu_stage_nums = 3
+      self.pred_stage_nums = 3
+
+      self.enc_channels = [32, 32, 64]
+      self.enc_resblk_nums = [2, 2, 2]
+
+      self.dec_enc_channels = [64, 128, 256, 512]
+      self.dec_enc_resblk_nums = [2, 4, 8, 2]
+      self.dec_dec_channels = [512, 256, 128, 64, 32, 32]
+      self.dec_dec_resblk_nums = [2, 4, 8, 2, 2, 2]
+
   if flags.name.lower() == 'vqvae_small':
     return VQVAEs(**flags)  # Small VQVAE with 2.5M parameters
   elif flags.name.lower() == 'vqvae_big':
     return VQVAEb(**flags)  # Big VQVAE with 4.6M parameters
   elif flags.name.lower() == 'vqvae_large':
     return VQVAEl(**flags)  # Large VQVAE with 8.0M parameters
+  elif flags.name.lower() == 'vqvae_huge':
+    return VQVAEh(**flags)  # Large VQVAE with 33.9M parameters
   elif flags.name.lower() == 'vae':
     return VAE(**flags)     # Default VQVAE with 4.2M parameters
   else:
