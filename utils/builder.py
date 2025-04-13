108ea1696d57a1e0102b456c3deff348e06aeabd
wst2001 <weisitong@pku.edu.cn>
2025-04-13 12:51:23 +0800

    Update VQVAEhuge

--- utils/builder.py
+++ utils/builder.py
@@ -34,16 +34,16 @@ def build_vae_model(flags):
   class VQVAEh(VQVAE):
     def config_network(self):
       self.bottleneck = 1
-      self.mpu_stage_nums = 3
-      self.pred_stage_nums = 3
+      self.mpu_stage_nums = 4
+      self.pred_stage_nums = 4
 
-      self.enc_channels = [32, 64, 128]
-      self.enc_resblk_nums = [2, 2, 2]
+      self.enc_channels = [32, 64, 128, 256]
+      self.enc_resblk_nums = [2, 2, 2, 2]
 
-      self.dec_enc_channels = [128, 256, 512, 1024]
+      self.dec_enc_channels = [256, 256, 512, 1024]
       self.dec_enc_resblk_nums = [2, 4, 4, 4]
-      self.dec_dec_channels = [1024, 512, 256, 128, 64, 32]
-      self.dec_dec_resblk_nums = [4, 4, 4, 2, 2, 2]
+      self.dec_dec_channels = [1024, 512, 256, 256, 128, 64, 32]
+      self.dec_dec_resblk_nums = [4, 4, 4, 2, 2, 2, 2]
 
   if flags.name.lower() == 'vqvae_big':
     return VQVAEb(**flags)  # Big VQVAE with 8.0M parameters
